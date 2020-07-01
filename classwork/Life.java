c2eab1ee609eff0bcdea18537a949481531663ea
Sangmin Pak <sangminpak2004@gmail.com>
2020-07-01 14:21:41 -0400

    I used bunch of if statements because 1. I'm not too comfortable with loops yet and 2. it helped me see what really happens before I lump them into a loop.
    I am currently working 'generate next gen cell' part.
    
    BTW TSEE HELPED ME FROM BASICALLY THE START! HE IS AMAZING! THANK YOU!

--- classwork/Life.java
+++ classwork/Life.java
@@ -49,15 +49,84 @@ class Life{
 	}
 	
 	public static int countNeighbours(char[][] board, int r, int c) {
-		return 0;
-	}
+		int value = 0 ;
+		
 
+		if ( r+1 >= 0 && r+1 < board.length &&  c >= 0 && c < board[r].length) {
+			if (board[r+1][c]=='X')
+			value = value + 1;
+		}
+		
+		if ( r-1 >= 0 && r-1 < board.length &&  c >= 0 && c < board[r].length) {
+			if (board[r-1][c]=='X')
+			value = value +1;
+		}
+		
+		if ( r >= 0 && r < board.length &&  c+1 >= 0 && c+1 < board[r].length) {
+			if (board[r][c+1]=='X')
+				value = value + 1;
+		}
+		
+		if ( r >= 0 && r < board.length &&  c-1 >= 0 && c-1 < board[r].length) {
+			if (board[r][c-1]=='X')
+				value = value + 1;
+		}
+		
+		if ( r+1 >= 0 && r+1 < board.length &&  c+1 >= 0 && c+1 < board[r].length){
+			if (board[r+1][c+1]=='X')
+				value = value + 1;
+		}
+		
+		if ( r+1 >= 0 && r+1 < board.length &&  c-1 >= 0 && c-1 < board[r].length){
+			if (board[r+1][c-1]=='X')
+				value = value + 1;
+		}	
+		
+		if ( r-1 >= 0 && r-1 < board.length &&  c+1 >= 0 && c+1 < board[r].length){
+			if (board[r-1][c+1]=='X')
+				value = value + 1;
+		}
+		
+		if ( r-1 >= 0 && r-1 < board.length &&  c-1 >= 0 && c-1 < board[r].length){
+			
+			if (board[r-1][c-1]=='X')
+				value = value + 1;
+		
+		}
+		
+		return value;
+	}
+	
+	public static char getNextGenCell(char[][] board, int r, int c){
+		int value = countNeighbours(board, r, c);
+		
+		if (value == 2 || value == 3) {
+			
+			board[r][c] = '5';
+			printBoard(board);
+		
+		}
+		
+	
+		return '0';
+	}
+	
+	
+
+	public static char[][] generateNextBoard(char[][] board) {
+		char newBoard[][] = new char[25][25];
+		
+		return newBoard;
+	}
 	
 	
 	public static void main(String[] args) {
 		char[][] board;
 		board = createNewBoard(25,25);
-		setCell(board, 3, 5, 'X'); 
+		setCell(board, 0, 24, 'X'); 
+		setCell(board, 0, 22, 'X');
+		setCell(board, 1, 24, 'X'); 		
+		System.out.println(countNeighbours(board, 0, 23));
 		printBoard(board);
 	}
 }
