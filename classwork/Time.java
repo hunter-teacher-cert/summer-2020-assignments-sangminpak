3fefc4af663e065e0b18a82e64a8389083134da2
Sangmin Pak <sangminpak2004@gmail.com>
2020-07-05 12:09:22 -0400

    took out main method

--- classwork/Time.java
+++ classwork/Time.java
@@ -18,6 +18,7 @@ public class Time{
 		this.minute = 0;
 		this.second = 0.0;
 		
+		//with no arguments, instance variables are set to zero.
 		//when all the instance variables are zero, it means midnight.
 		//you do not declare "this" and you can't make an assignment to it.
 		//i guess like point or rectangle?
@@ -40,26 +41,76 @@ public class Time{
 		
 	}
 	
+	public boolean equals (Time that) {
+		return this.hour == that.hour
+		&& this.minute == that.minute
+		&& this.second == that.second; 
+	}
+	
 public static Time add(Time t1, Time t2) {
 		Time sum = new Time();
 		sum.hour = t1.hour + t2.hour;
 		sum.minute = t1.minute + t2.minute;
 		sum.second = t1.second + t2.second;
-		return sum;
+		
+		if (sum.second >= 60.0) {
+		sum.second -= 60.0;
+		sum.minute +=1;
+	}
+	
+	if (sum.minute >= 60) {
+		sum.minute -= 60;
+		sum.hour += 1;
 		
 	}
+	
+	return sum;
+}
     
+public Time add(Time t2) {
+	Time sum = new Time();
+	sum.hour = this.hour + t2.hour;
+	sum.minute = this.minute + t2.minute;
+	sum. second = this.second + t2.second;
+	
+	if (sum.second >= 60.0) {
+		sum.second -= 60.0;
+		sum.minute +=1;
+	}
+	
+	if (sum.minute >= 60) {
+		sum.minute -= 60;
+		sum.hour += 1;
+	}
+	
+	return sum;
+	
+}
     
-public static void main(String[] args) {
-	Time time = new Time (10, 20, 30.0);
-	printTime(time);
-	Time startTime = new Time(18,50, 0.0);
-	Time runningTime = new Time(2, 16, 0.0);
-	Time endTime = Time.add(startTime, runningTime);
-	printTime(add(startTime, runningTime));
+	
+	public void increment(double seconds) {
+		this.second += seconds;
+		while (this.second >= 60.0) {
+			this.second -= 60.0;
+			this.minute += 1;
+		}
+		
+		while (this.minute >= 60) {
+			this.minute -= 60;
+			this.hour += 1;
+		}
+	}
+	
+//public static void main(String[] args) {
+	//Time time = new Time (10, 20, 30.0);
+	//printTime(time);
+	//Time startTime = new Time(18,50, 0.0);
+	//Time runningTime = new Time(2, 16, 0.0);
+	//Time endTime = Time.add(startTime, runningTime);
+	//printTime(add(startTime, runningTime));
 	
 	
 }
 
-	}
+	//}
 
