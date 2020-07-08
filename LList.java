62c9a052c8e44236ea0798deaf990968fbbed932
sangminpak <67596997+sangminpak@users.noreply.github.com>
2020-07-08 12:59:00 -0400

    Add files via upload
    i want to start my repo all over...merging issues keep...emerging...

---
+++ LList.java
@@ -0,0 +1,79 @@
+
+import java.io.*;
+import java.util.*;
+
+public class LList{
+	private Node head;
+
+	
+	public LList(){
+		head = null;
+	
+	}
+	
+
+	
+	public void add(String data){
+		Node newnode = new Node(data);
+		newnode.setNext(head);
+		head = newnode;
+		//basically it points back to itself so that it can refer to itself.
+		//therefore head is not null anymore. It's whatever the data is.
+	}
+	
+	public String toString(){
+		String s = "";
+		
+		Node currentNode;
+		currentNode = head;
+		//we're creating a temporary node, which is now head.
+		while (currentNode !=null){
+			//so using the add method, the head is probably not null (Because it's has newnode data)
+			//so if head is not null, currentNode is not null.
+			
+			s = s + currentNode.getData()+"-->";
+			//writes itself + get whatever currentNode's data is, then...
+			
+			currentNode = currentNode.getNext();
+			
+			//gets whatever comes after currentNode, then continues the loop
+		
+		}
+		
+		s = s + "null";
+		return s;
+		
+	}
+	
+	public boolean isEmpty() {
+		//if (head == null) {
+			//return true;
+		//}
+		
+		//return false;
+		//the above code is kind of redundant, so it can be more concise like below:
+		
+		return head == null ;
+		
+	
+	}
+	
+	public String get(int index){
+		int count = 0;
+		Node currentNode = this.head;
+		while (count != index && currentNode.getData() != null) {
+			if(currentNode.getNext() == null) {
+				return null;
+			}
+			else{
+				currentNode=currentNode.getNext();
+				count++;
+			}
+			
+		}
+		return currentNode.getData();
+	}	
+		
+	
+}
+
