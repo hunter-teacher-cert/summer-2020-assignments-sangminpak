21127e00600e1fc76dc63b549e4724637f8c1de7
sangminpak <67596997+sangminpak@users.noreply.github.com>
2020-07-13 06:51:16 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,5 +1,3 @@
-
-
 import java.io.*;
 import java.util.*;
 
@@ -8,33 +6,17 @@ public class Driver{
 
 	public static void main(String[] args) {
 		
-	LList l = new LList();
+	Stack newStack = new Stack ();
 	
-	try { 
-		l.addFront(null);
-	}
+	newStack.push("Hello");
+	newStack.push("My name is");
+	newStack.push("Spongebob");
+	System.out.println(newStack);
 	
-	catch (IllegalArgumentException e ) {
-		System.out.println("Element not added");
-	}
-	l.addFront("Hello");
-	l.addFront("My name is");
-	l.addFront("Spongebob");
-	System.out.println(l);
+	System.out.println(newStack.isEmpty());
 	
-	System.out.println(l.isEmpty());
 	
-	System.out.println(l.get(4));
-	
-	//l.set(1, "yo");
-	//System.out.println(l);
-	
-	//l.insert(1, "what");
-	//System.out.println(l);
-	
-	//System.out.println("Your keyword is found at index: " + l.search("word")); --> testing NoSuchElementException
-	
-}
 
 }
 
+}
