6f363de5a62e7899e3eab104f2a178c90515239a
sangminpak <67596997+sangminpak@users.noreply.github.com>
2020-07-13 06:50:49 -0400

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,25 +1,51 @@
 import java.io.*;
 import java.util.*;
 
-public CLass MyStack{
-      Node top;
+public class Stack{
+     
+	 private Node top;
       
 
 
-      public myStack(){
+     public Stack(){
          top = null;
-      }
+		 
+		      }
+			  
+	public String toString(){
+		String s = "";
+		
+		Node currentNode;
+		currentNode = top;
+		//we're creating a temporary node, which is now head.
+		while (currentNode !=null){
+			//so using the add method, the head is probably not null (Because it's has newnode data)
+			//so if head is not null, currentNode is not null.
+			
+			s = s + currentNode.getData() + "\n";
+			//writes itself + get whatever currentNode's data is, then...
+			
+			currentNode = currentNode.getNext();
+			
+			//gets whatever comes after currentNode, then continues the loop
+		
+		}
+		
+		s = s + "null";
+		return s;
+		
+	}
 
       public void push(String data){
           Node newnode = new Node(data);
-          newnode.next = top; // i'm not sure if this makes more sense than newnode.setNext. I'll just test it out!
+          newnode.setNext(top); // i'm not sure if this makes more sense than newnode.setNext. I'll just test it out!
           top = newnode;
           
           //if (top == null) {
          // top = newnode; Is this part necessary? It makes sense in my head but....
-      }
+     }
 
-      public String pop(){
+     // public String pop(){
           // add code to remove and return the
           // item on the top of the stack
           //1. get the data of the top element, 2. have the top point to the next element, 3. top is next element.
@@ -28,10 +54,10 @@ public CLass MyStack{
           //top.getData();
           //top = top.setNext;
           //return data;
-      }
+      //}
 
-      public String top(){
-      return top.data; //????? is this it???
+     public String top(){
+		return top.getData(); //????? is this it???
          
          
          
@@ -43,11 +69,11 @@ public CLass MyStack{
           return top == null;
       }
 
-      public int size(){
+      //public int size(){
      
           // add code to return the number of items currently on the stack
           
           //this is probably similar to length() where it just counts until the end of the list.
-      }
+      //}
     
   }
