aa14e36a840affcb2b24a9e7aba7ee5bcd4560a1
sangminpak <67596997+sangminpak@users.noreply.github.com>
2020-07-20 13:51:43 -0400

    Create BSTree.java
    the if statements must not be complete, because it keeps giving me NullPointerException. I need to trace it so that I can point out exactly why it's wrong, but it made total sense when I wrote the pseudocode lol I put System.out.println so that I can see what's the current data after if statement, and it seems like currentData is not being updated! Do I have to do something like current.getRight().getData()???

---
+++ day14/BSTree.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class BSTree {
+	private TreeNode root;
+	
+	public BSTree(){
+		root = null;
+	}
+	
+	public void seed(){
+		TreeNode t;
+		
+		t = new TreeNode(10);
+		//new treenode made with 10 as data and left and right are null
+		
+		root = t;
+		t = new TreeNode(5);
+		root.setLeft(t);
+		t = new TreeNode(20);
+		root.setRight(t);
+		//I think this part is setting 5 for left and 20 for right
+		//but i'm confused about why we're using t for all
+		//i guess it doesn't matter??
+		
+		root.getLeft().setRight(new TreeNode(8));
+		//left's right(this is a child) is 8
+		
+		t = new TreeNode(15);
+		root.getRight().setLeft(t);
+		
+		t = new TreeNode(22);
+		root.getRight().setRight(t);
+	
+	}
+	
+	public int search(int key){
+		TreeNode current = root;
+		int currentData = current.getData();
+		while (currentData != key){
+		
+			if (key < currentData) {
+				current = current.getLeft();
+				System.out.println(currentData);
+			}
+			if (key > currentData) {
+				current = current.getRight();
+				System.out.println(currentData);
+			}
+
+		
+			
+		}
+		return currentData;
+	}
+	
+	
+	
+
+
+
+
+}
