daae3f6863ba0e8666251591ef10a62c3d3277ca
sangminpak <67596997+sangminpak@users.noreply.github.com>
2020-07-22 08:31:15 -0400

    Create PhoneBook.java
    braindump still

---
+++ day15/PhoneBook.java
@@ -0,0 +1,37 @@
+ public class PhoneBook{
+ /*
+ add();
+ add() should take in the person's full name as well as their phone number.
+ I want something to look like:
+ add(lastName, firstName, phoneNumber)
+ Inside the method, I want it to look like:
+ 
+ int[] lastNameArray;
+ lastNameArray[lastName.charAt(0)], which means like you're basically going to
+ A section or F section in that phone book,
+ and then it stores that person's full last name, the first name and phone #.
+ But can a node store multiple things?
+ maybe if we do something like Node(String lastName, String firstName, String phoneNumber).....
+ and then something like this.lastName = lastName; this.firstName = firstName; this.phoneNumber=phoneNumber;
+ 
+ 
+ 
+ 
+ find();
+ if we are ONLY using the names to find the person,
+ first it should look at lastName.charAt(0);
+ that should tell the program which index to look.
+ Then, it can go down the linked list to find if the values inside the nodes are equal..
+ So some kind of for loop here... or while loop..?
+ 
+ remove();
+ basically use LList remove() to have the node skip one and connect to the next
+ 
+ printList();
+ while traversing the array, it would go down the linked list and print the values stored in
+ the variables in the order.
+ 
+ */
+ 
+ }
+ 
